88d5cb025cfcbf2d335f3371925f4144b0f8a4d3
futuresea-dev <futuresea.dev@gmail.com>
2024-07-24 20:20:11 +0200

    chore: optimize clients logos; use auto scroll on carousel

--- apps/frontend/src/views/IndexView/parts/Clients/Clients.Carousele.tsx
+++ apps/frontend/src/views/IndexView/parts/Clients/Clients.Carousele.tsx
@@ -1,35 +1,41 @@
-import Autoplay from 'embla-carousel-autoplay'
+import { type EmblaOptionsType } from 'embla-carousel'
+import AutoScroll from 'embla-carousel-auto-scroll'
 import useEmblaCarousel from 'embla-carousel-react'
-import React, { useCallback, useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 type ClientsCarouselePropsType = {
   clientLogos: string[]
 }
 
+const options: EmblaOptionsType = {
+  loop: true,
+  align: 'start',
+  watchDrag: false,
+}
+
 export const ClientsCarousele: React.FC<ClientsCarouselePropsType> = ({ clientLogos }) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, dragFree: false, slidesToScroll: 1, skipSnaps: false }, [
-    Autoplay({
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    AutoScroll({
       playOnInit: true,
+      startDelay: 0,
+      speed: 1,
+      stopOnInteraction: false,
     }),
   ])
 
-  const autoplay = useCallback(() => {
-    if (!emblaApi) return
-    emblaApi.scrollNext()
-  }, [emblaApi])
-
   useEffect(() => {
-    if (!emblaApi) return
-    const autoplayInterval = setInterval(autoplay, 4000) // Adjust the interval to control speed
-    return () => clearInterval(autoplayInterval)
-  }, [autoplay, emblaApi])
+    const autoScroll = emblaApi?.plugins()?.autoScroll
+    if (!autoScroll) return
+    if (autoScroll.isPlaying() === true) return
+    autoScroll.play()
+  }, [emblaApi])
 
   return (
     <div className="overflow-hidden w-full" ref={emblaRef}>
       <div className={'flex items-center gap-[80px]'}>
         {[...clientLogos, ...clientLogos, ...clientLogos, ...clientLogos, ...clientLogos].map((logo, index) => (
-          <div className={'relative shrink-0 basis-[200px]'} key={index}>
-            <img src={logo} />
+          <div className={'relative shrink-0 grow-0 max-w-full'} key={index}>
+            <img src={logo} loading="lazy" decoding="async" className="select-none pointer-events-none" />
           </div>
         ))}
       </div>
