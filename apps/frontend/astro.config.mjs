b871e915c207623561827a7456473e30e0248bde
futuresea-dev <futuresea.dev@gmail.com>
2024-09-08 18:10:48 +0200

    chore: cleanup, deps update

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,30 +1,26 @@
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import paraglide from '@inlang/paraglide-astro'
-import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import paraglide from '@inlang/paraglide-astro';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flightcore.raqz.link',
   i18n: {
     defaultLocale: 'pl',
-    locales: ['pl', 'en'],
+    locales: ['pl', 'en']
   },
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.mjs',
-    }),
-    react(),
-    icon({
-      iconDir: './src/icons',
-    }),
-    sitemap({}),
-    paraglide({
-      project: './project.inlang',
-      outdir: './src/paraglide', //where your files should be
-    }),
-  ],
-  output: 'static',
-})
+  integrations: [tailwind({
+    configFile: './tailwind.config.mjs'
+  }), react(), icon({
+    iconDir: './src/icons'
+  }), sitemap({}), paraglide({
+    project: './project.inlang',
+    outdir: './src/paraglide' //where your files should be
+  }), mdx()],
+  output: 'static'
+});
--- apps/frontend/tailwind.config.mjs
+++ apps/frontend/tailwind.config.mjs
@@ -1,12 +1,10 @@
 import tailwindConfigBase from '@flightcore/uikit/tailwind.config.mjs'
-
-console.log(require.resolve('@flightcore/uikit'))
+import merge from 'lodash/merge'
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  ...tailwindConfigBase,
+export default merge(tailwindConfigBase, {
   content: [
     'src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     '../../libs/uikit/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
-}
+})
