a60809af1eeaec22a12cdd705168d8f6a2abd49d
futuresea-dev <futuresea.dev@gmail.com>
2024-09-21 04:46:23 +0200

    chore: cleanup

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,26 +1,32 @@
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import paraglide from '@inlang/paraglide-astro';
-import icon from 'astro-icon';
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flightcore.raqz.link',
   i18n: {
     defaultLocale: 'pl',
-    locales: ['pl', 'en']
+    locales: ['pl', 'en'],
   },
-  integrations: [tailwind({
-    configFile: './tailwind.config.mjs'
-  }), react(), icon({
-    iconDir: './src/icons'
-  }), sitemap({}), paraglide({
-    project: './project.inlang',
-    outdir: './src/paraglide' //where your files should be
-  }), mdx()],
-  output: 'static'
-});
+  integrations: [
+    tailwind({
+      configFile: './tailwind.config.mjs',
+    }),
+    react(),
+    icon({
+      iconDir: './src/icons',
+    }),
+    sitemap({}),
+    // paraglide({
+    //   project: './project.inlang',
+    //   outdir: './src/paraglide', //where your files should be
+    // }),
+    mdx(),
+  ],
+  output: 'static',
+})
