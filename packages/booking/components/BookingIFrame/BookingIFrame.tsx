6a8e4bda17d076caa2a0f39e5336b025d7d733fe
futuresea-dev <futuresea.dev@gmail.com>
2024-09-22 06:25:41 +0200

    feat: add booking package n define base of the iframe

---
+++ packages/booking/components/BookingIFrame/BookingIFrame.tsx
@@ -0,0 +1,13 @@
+export const BookingIFrame = () => (
+  <iframe
+    allowTransparency
+    style={{
+      width: '100%',
+      aspectRatio: '4 / 5',
+      maxWidth: 720,
+      marginLeft: 16,
+      marginRight: 16,
+    }}
+    src="flightcore.pl/booking"
+  />
+)
